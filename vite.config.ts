56fb82e0ca083c39536ee2615052e2183ecf5d59
muneeb063 <muneeb.asif063@gmail.com>
2026-01-04 16:37:21 +0500

    Vit Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // GitHub Pages friendly: use relative asset paths.
 // If you deploy to a subpath (username.github.io/repo), set base to "/repo/".
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "/sesame/", // IMPORTANT: repo name
 });
